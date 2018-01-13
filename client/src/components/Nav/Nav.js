42845520d659a6ddb9fbfed4d6226e7cb44deee0
Akiyo <flying.unicorn.sf@gmail.com>
2018-01-13 12:49:49 -0800

    page navigation initiated

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,35 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Nav.css';
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
-const Nav = props =>
+const Nav = props =>(
   <nav className="navbar navbar-default">
     <div className="container-fluid">
-      <div className="navbar-header">
-        <Link className="navbar-brand" to="/">
-          Kinder
-        </Link>
-      </div>
       <ul className="nav navbar-nav">
         <li
           className={
             window.location.pathname === "/" ||
-            window.location.pathname === "/about"
+            window.location.pathname === "/home"
               ? "active"
               : ""
           }
         >
-          <Link to="/">About</Link>
+          <Link to="/">Home</Link>
         </li>
         <li
-          className={window.location.pathname === "/discover" ? "active" : ""}
-        >
-          <Link to="/discover">Discover</Link>
+          className={window.location.pathname === "/match" ? "active" : ""}>
+          <Link to="/match">Match</Link>
         </li>
         <li className={window.location.pathname === "/search" ? "active" : ""}>
           <Link to="/search">Search</Link>
         </li>
+        <li className={window.location.pathname === "/logout" ? "active" : "logout"}>
+          <Link to="/">LogOut</Link>
+        </li>
       </ul>
     </div>
-  </nav>;
-  
+  </nav>
+);  
 export default Nav;
