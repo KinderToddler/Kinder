fdc497cdfa7789b344117269983a6b05002a7dd2
Akiyo <flying.unicorn.sf@gmail.com>
2018-01-19 20:14:39 -0800

    trying to get the status lined up.

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,23 +1,32 @@
 import React from "react";
-import { Link, Route } from "react-router-dom";
+// import { Route } from "react-router-dom";
 import './Profile.css';
 import ProfilePic from "../ProfilePic/ProfilePic";
 import Name from "../Name/Name";
 import Height from "../Height/Height";
 import Age from "../Age/Age";
-import Likes from "../Likes/Likes";
-import Dislikes from "../Dislikes/Dislikes";
-import Allergies from "../Allergies/Allergies";
+import Text from "../Text/Text";
 
-    // <pre>
+    //  <pre>
     //   { JSON.stringify(props, null, 2) }
     // </pre>
+
 const Profile = (props) =>(
   <div className="profile-container">
+       <pre>
+      { JSON.stringify(props, null, 2) }
+    </pre>
     <ProfilePic img={props.profile.imgUrl} />
     <div className="user-info">
         <div className="user-name">
-            Name: <Name name={props.profile.firstName} />
+            First Name: <Name name={props.profile.firstName} />
+            Last Name: <Name name={props.profile.lastName} />
+        </div>
+        <div className="user-name">  
+            Username: <Name name={props.profile.username} />
+        </div>
+        <div className="user-email">  
+            Email: <Name name={props.profile.email} />
         </div>
         <div className="user-height">
             Height: <Height height={props.profile.height} />
@@ -26,16 +35,13 @@ const Profile = (props) =>(
             Age: <Age age={props.profile.age} />
         </div>
         <div className="user-likes">   
-            Likes: <Likes likes={props.profile.likes} />
+            Likes: <Text text={props.profile.likes} />
         </div>
         <div className="user-dislikes">   
-            Dislikes: <Dislikes dislikes={props.profile.dislikes} />
+            Dislikes: <Text text={props.profile.dislikes} />
         </div>
         <div className="user-allergies">  
-            Allergies: <Allergies allargies={props.profile.allergies} />
-        </div>
-        <div className="user-name">  
-            Username: <Allergies username={props.profile.username} />
+            Allergies: <Text text={props.profile.username} />
         </div>
     </div>
   </div>  
